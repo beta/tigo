e0246d836b19e8b6c3e355f9a50f510dede492d4
Beta Kuang <beta.kuang@gmail.com>
2018-03-21 22:17:31 +0800

    create project

---
+++ README.md
@@ -0,0 +1,7 @@
+# TIGO
+
+TIGO is a Go binding for [TIGR](https://bitbucket.org/rmitton/tigr), a tiny graphics library.
+
+## License
+
+MIT
---
+++ tigo.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2018 Beta Kuang
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// Package tigo implements the interface for TIGO.
+package tigo
