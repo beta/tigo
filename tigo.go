03ad6c03380e7bf737ac3c514ce569ca218cd5d9
Beta Kuang <beta.kuang@gmail.com>
2018-03-22 08:29:14 +0800

    add drawing functions

--- tigo.go
+++ tigo.go
@@ -20,8 +20,6 @@ type Pixel struct {
 	A uint8
 }
 
-type tPixel _Ctype_TPixel
-
 // WindowFlag reprensets a flag for window sizing mode.
 type WindowFlag int
 
@@ -95,24 +93,90 @@ func (bmp *Bitmap) SetPostFX(hBlur, vBlur bool, scanlines, contrast float32) {
 
 // Drawing
 
-// Get gets a pixel from bitmap.
-func (bmp *Bitmap) Get(x, y int) Pixel {
-	cPixel := C.tigrGet((*C.Tigr)(bmp.cBitmap), C.int(x), C.int(y))
+// goPixel converts a TPixel from C into a Pixel from Go.
+func goPixel(p C.TPixel) Pixel {
 	return Pixel{
-		B: uint8(cPixel.b),
-		G: uint8(cPixel.g),
-		R: uint8(cPixel.r),
-		A: uint8(cPixel.a),
+		B: uint8(p.b),
+		G: uint8(p.g),
+		R: uint8(p.r),
+		A: uint8(p.a),
 	}
 }
 
-// Plot sets a pixel onto bitmap.
-func (bmp *Bitmap) Plot(x, y int, p Pixel) {
-	cPixel := tPixel{
+// tPixel converts a Pixel from Go into a TPixel from C.
+func cPixel(p Pixel) C.TPixel {
+	return C.TPixel{
 		b: C.uchar(p.B),
 		g: C.uchar(p.G),
 		r: C.uchar(p.R),
 		a: C.uchar(p.A),
 	}
-	C.tigrPlot((*C.Tigr)(bmp.cBitmap), C.int(x), C.int(y), C.TPixel(cPixel))
+}
+
+// RGB is a helper function for making colors.
+func RGB(r, g, b uint8) Pixel {
+	return Pixel{
+		R: r,
+		G: g,
+		B: b,
+		A: 0xff,
+	}
+}
+
+// RGBA is a helper function for making colors with alpha value.
+func RGBA(r, g, b, a uint8) Pixel {
+	return Pixel{
+		R: r,
+		G: g,
+		B: b,
+		A: a,
+	}
+}
+
+// Get gets a pixel from bitmap.
+func (bmp *Bitmap) Get(x, y int) Pixel {
+	return goPixel(C.tigrGet((*C.Tigr)(bmp.cBitmap), C.int(x), C.int(y)))
+}
+
+// Plot sets a pixel onto bitmap.
+func (bmp *Bitmap) Plot(x, y int, p Pixel) {
+	C.tigrPlot((*C.Tigr)(bmp.cBitmap), C.int(x), C.int(y), cPixel(p))
+}
+
+// Clear clears a bitmap to a color.
+func (bmp *Bitmap) Clear(color Pixel) {
+	C.tigrClear((*C.Tigr)(bmp.cBitmap), cPixel(color))
+}
+
+// Fill fills in a solid rectangle.
+func (bmp *Bitmap) Fill(x, y, w, h int, color Pixel) {
+	C.tigrFill((*C.Tigr)(bmp.cBitmap), C.int(x), C.int(y), C.int(w), C.int(h), cPixel(color))
+}
+
+// Rect draws an empty rectangle (exclusive coords).
+func (bmp *Bitmap) Rect(x, y, w, h int, color Pixel) {
+	C.tigrRect((*C.Tigr)(bmp.cBitmap), C.int(x), C.int(y), C.int(w), C.int(h), cPixel(color))
+}
+
+// Line draws a line.
+func (bmp *Bitmap) Line(x0, y0, x1, y1 int, color Pixel) {
+	C.tigrLine((*C.Tigr)(bmp.cBitmap), C.int(x0), C.int(y0), C.int(x1), C.int(y1), cPixel(color))
+}
+
+// Blit copies bitmap data from bmp to dest.
+// sx/sy = source coordinates.
+// dx/dy = dest coordinates.
+// w/h: width/height.
+func (bmp *Bitmap) Blit(sx, sy int, dest *Bitmap, dx, dy, w, h int) {
+	C.tigrBlit((*C.Tigr)(dest.cBitmap), (*C.Tigr)(bmp.cBitmap), C.int(dx), C.int(dy), C.int(sx), C.int(sy), C.int(w), C.int(h))
+}
+
+// BlitAlpha is same as Blit, but blends with the bitmap alpha channel, and uses the 'alpha' variable to fade out.
+func (bmp *Bitmap) BlitAlpha(sx, sy int, dest *Bitmap, dx, dy, w, h int, alpha float32) {
+	C.tigrBlitAlpha((*C.Tigr)(dest.cBitmap), (*C.Tigr)(bmp.cBitmap), C.int(dx), C.int(dy), C.int(sx), C.int(sy), C.int(w), C.int(h), C.float(alpha))
+}
+
+// BlitTint is same as Blit, but tints the source bitmap with a color.
+func (bmp *Bitmap) BlitTint(sx, sy int, dest *Bitmap, dx, dy, w, h int, tint Pixel) {
+	C.tigrBlitTint((*C.Tigr)(dest.cBitmap), (*C.Tigr)(bmp.cBitmap), C.int(dx), C.int(dy), C.int(sx), C.int(sy), C.int(w), C.int(h), cPixel(tint))
 }
